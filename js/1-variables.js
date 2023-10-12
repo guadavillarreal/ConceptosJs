e88e9bfe91a3c402c0cb42fa927b4b5c28a696b7
guadavillarreal <guada.villarreal.gnv@gmail.com>
2023-10-12 18:32:56 -0300

    agregamos un comentario

---
+++ js/1-variables.js
@@ -0,0 +1,9 @@
+//para los comentarios
+/*Esto es un comentario
+de carias lineas
+*/ 
+//declarar una variable (var - let - const)
+/*let: puedo canviar el valor  
+const: no permite canviar el valor 
+= operador de asignacion, */
+let anioActual = 2023
