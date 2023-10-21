628675d91c8a23aeafdafe02e0051130b1ed60d4
guadavillarreal <guada.villarreal.gnv@gmail.com>
2023-10-20 22:59:58 -0300

    inicio de condicionales, -if-

---
+++ js/2-condicionales.js
@@ -0,0 +1,14 @@
+/* if(condicion logica){
+    codigo a ejecutar si se cumple la condicion logica
+}
+*/
+//7- Escriba un programa que pida 3 numeros y escriba en la pantalla el mayor de los 3
+
+let numero1= parseInt(prompt("Ingrese un primer número"));
+let numero2= parseInt(prompt("Ingrese un segundo número"));
+let numero3= parseInt(prompt("Ingrese un tercer número"));
+
+if(numero1>numero2){
+    console.log("La variable numero 1 es mayor que la variable 2")
+}
+//hacer ejer hasta condicionales
