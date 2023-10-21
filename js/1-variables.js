b219c919d9e0b7e40b54fbcf9d984f3ceaf49d46
guadavillarreal <guada.villarreal.gnv@gmail.com>
2023-10-20 21:46:26 -0300

    correccion de espacios

--- js/1-variables.js
+++ js/1-variables.js
@@ -3,40 +3,41 @@
 de varias lineas*/
 
 /*declarar una variable (var - let - const) var actualmente no se la utiliza
-- let: puedo cambiar el valor  
-- const: no permite canviar el valor 
+- let: puedo cambiar el valor , tienen que empezar con la primera letra minuscula 
+- const: no permite cambiar el valor 
         = operador de asignacion, */
-let anioActual = 2023
-const url = "https://universe.rollingcodeschool.com/live/videos/64b5c70978c18f053461e4b5"
+let anioActual = 2023;
+const url =
+  "https://universe.rollingcodeschool.com/live/videos/64b5c70978c18f053461e4b5";
 
-//mostrar mensajes
-/*consol log  */
-console.log('Este es un mendaje de consola');
+//en los txt debo usar comillas simples o dobles no el txto suelto! lo reconoce como error
+/*para mostrar msj por consola utilizo un consol log  */
+console.log("Este es un mendaje de consol");
 console.log(anioActual);
 console.log(url);
-console.log('Año: '+(anioActual+2));
+console.log("Año: " + (anioActual + 2));
 
 //modificar el contenido de una variable
-anioActual=2024;
+anioActual = 2024;
 console.log(anioActual);
 
 //url='otro valor'; no se puede hacer porq declare como variable constante
 console.log(url);
 
 //para que js identifique las etiquetas de HTML
-document.write('<h1>Esto es un mensaje por pantalla</h1>')
-document.write('año: '+ anioActual + '<br>');
-document.write('Año: '+anioActual);
+document.write("<h1>Esto es un mensaje por pantalla</h1>");
+document.write("año: " + anioActual + "<br>");
+document.write("Año: " + anioActual);
 
 //ventanas emergentes
-alert('Esto es una prueba de alerta');
+alert("Esto es una prueba de alerta");
 
 //si quiero pedirle algo al us uso un prompt
-let color = prompt('Ingrese un color');
+let color = prompt("Ingrese un color");
 console.log(color); //para verificar que el us ingrese loq le pido
 
 //solicitar al us un nemro, y al nuemro indicado por elus multiplicarle 15
-let numeroSeleccionado=prompt("Ingrese una numero de 1 a 100");
+let numeroSeleccionado = prompt("Ingrese una numero de 1 a 100");
 console.log(numeroSeleccionado);
 console.log(numeroSeleccionado + 15);
 
