e449755b1d645553d72d387579ab437ebf33561e
guadavillarreal <guada.villarreal.gnv@gmail.com>
2023-10-24 17:16:19 -0300

    resolucion del ejer 6.1

--- js/2-condicionales.js
+++ js/2-condicionales.js
@@ -3,19 +3,24 @@
 }
 */
 //7- Escriba un programa que pida 3 numeros y escriba en la pantalla el mayor de los 3
-//7- Escriba un programa que pida 2 numeros y escriba en la pantalla el mayor de los 2
+//6- Escriba un programa que pida 2 numeros y escriba en la pantalla el mayor de los 2
 
 let numero1 = parseInt(prompt("Ingrese un primer número"));
 let numero2 = parseInt(prompt("Ingrese un segundo número"));
 let numero3 = parseInt(prompt("Ingrese un tercer número"));
 
 if (numero1 > numero2) {
-  console.log("El número " + numero1 + " es mayor que el número " + numero2);
+  document.write("El número " + numero1 + " es mayor que el número " + numero2);
 } else {
   if (numero2 > numero1) {
-    console.log("El número " + numero2 + " es mayor que el número " + numero1);
+    document.write("El número " + numero2 + " es mayor que el número " + numero1);
   } else {
-    console.log("Los números ingresados son iguales ( " + numero2 + " )");
+    document.write("Los números ingresados son iguales ( " + numero2 + " )");
   }
 }
 //hacer ejer hasta condicionales
+/*puedo para concatenar sin abrir y cerrar las comillas a cada rato puede ser:` - con el alt de la dercha del teclado -
+y para que concatene una variable ${variable}
+por ejemplo:
+    document.write(`El número  ${numero2} es mayor que el número ${numero1}`);
+             */
